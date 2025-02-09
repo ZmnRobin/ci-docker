e5e9f153f3c5b93a78c08789ef85999e228e3c3b
ZmnRobin <robinmilford38@gmail.com>
2025-02-09 13:45:15 +0600

    minor changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express();
 const PORt=5000;
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    res.send('Hello! This is a simple express test server.');
 });
 
 app.listen(PORt,()=>{
